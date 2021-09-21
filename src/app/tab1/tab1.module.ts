b54e79332db5b42419c7d3d17646bf1c79e15f36
Vinicio <viniciojoj@gmail.com>
2021-09-20 23:26:23 -0600

    Cambios al home

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -4,7 +4,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
+import { SMS } from '@ionic-native/sms/ngx';
+import { SmsRetriever } from '@ionic-native/sms-retriever/ngx';
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 
 @NgModule({
@@ -15,6 +16,7 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     ExploreContainerComponentModule,
     Tab1PageRoutingModule
   ],
+  providers: [SMS, SmsRetriever],
   declarations: [Tab1Page]
 })
 export class Tab1PageModule {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { SMS } from '@ionic-native/sms/ngx';
+import { SmsRetriever } from '@ionic-native/sms-retriever/ngx';
+
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +9,67 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  public smsTextmessage: string = '';
+  public appHashString: string = '';
+  public isOn:boolean = false;
+  constructor(private sms: SMS, private smsRetriever: SmsRetriever) {
+    this.getHashCode();        
+  }
 
-  constructor() {}
+  verifyStatus() 
+  {
+    this.sms.send('48135997', `status  #${this.appHashString}#`);
+  }
 
+  encenderMotor() {
+    this.sms.send('48135997', `on #${this.appHashString}#`);
+    this.isOn = true;
+  }
+
+
+  apagarMotor() {
+    this.sms.send('48135997', `off #${this.appHashString}#`);
+    this.isOn = false;
+  }
+
+
+  getHashCode() {
+    this.smsRetriever.getAppHash()
+      .then((res: any) => {
+        this.appHashString = res;        
+        this.verifyStatus();
+        this.getSMS();
+      })
+      .catch((error: any) => console.error(error));
+  }
+
+  getSMS() {
+    this.smsRetriever.startWatching()
+      .then((res: any) => {
+        this.smsTextmessage = res.Message;
+        
+        var texto = this.smsTextmessage.split(" ");
+        var comando = texto[0].split(":");
+        console.log(comando);
+        var respuesta = comando[1];
+        console.log(respuesta);
+        switch (comando[0]) 
+        {
+          case "status":
+            this.isOn = (respuesta.trim() == "on");
+          break;
+        }
+
+      })
+      .catch((error: any) => console.error(error))
+      .finally(() => {
+        this.getSMS();
+      });
+  }
 }
+
+// comando  #key#
+
+// comando:respuesta #key#
+
+// comando:{json} #key#
